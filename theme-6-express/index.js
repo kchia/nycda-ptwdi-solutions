70ea78ddd845da1fa61fafbf965948a311f5adfb
kchia <kchia87@gmail.com>
2017-09-14 10:35:50 -0400

    Add express lecture notes

--- theme-6-express/index.js
+++ theme-6-express/index.js
@@ -1,18 +1,92 @@
 var express = require('express');
 
-// create an expres application
+// create an express application
 var app = express();
 
+var fs = require('fs');
+
+var parser = require('body-parser');
+
+//parses requests with the content type of `application/json`
+// constructs request.body for every incoming request
+app.use(parser.json());
+
+//if we wanted to forbid query string usage, that could be done in the middleware.
+// app.use(function(request, response, next){
+//   var query = request.query;
+
+//   if(Object.keys(query).length > 0) {
+//     response.status(400).send('You cannot use query strings');
+//   } else {
+//     next();
+//   }
+// });
+
+/**
+  EXERCISE: MIDDLEWARE
+  - Create middleware for the earlier examples to make a log of incoming requests.   Include the original route and a UTC timestamp. Have the log write to a file called "log.txt" in your project directory.
+  - Hint: the original route for the request is on request.originalUrl.
+**/
+app.use(function(request, response, next) {
+  // Create timestamp
+
+  // Return the number of milliseconds since 1970/01/01:
+  var now = new Date();
+  var utcDate = now.toUTCString();
+
+  // create log content
+  var logContent = `Original route: ${request.originalUrl}, \nTimestamp: ${utcDate}\n`;
+
+  fs.appendFile('./log.txt', logContent, function(error){
+
+    if (error) throw error;
+
+    console.log('Successfully created a log entry!');
+  } );
+
+  next();
+});
+
+app.post('/save', function(request, response) {
+  console.log(request.body);
+  response.send('save request received');
+});
+
+/**
+  MULTIPLE ROUTE NAMES (5 minutes)
+**/
+
+app.get(['/hello', '/hi', '/hola'], function(request, response) {
+  response.send('hello and hi and hola!');
+});
+
+
+/**
+  EXERCISE: ROUTING (10 minutes)
+  - Create a route that takes a word as a param and return the word reversed
+**/
+app.get('/reverse/:word', function(request, response) {
+  var word = request.params.word; // 'hello'
+  // word.split('') -> ['h', 'e', 'l', 'l', 'o'];
+  // word.split('').reverse() -> ['o', 'l', 'l', 'e', 'h']
+  // word.split('').reverse().join('') -> 'olleh'
+  var reversedWord = word.split('').reverse().join('');
+
+  response.send(reversedWord);
+
+});
+
+
 app.get('/hello/world', function(request, response){
-	console.log('got request for "/hello/world"');
-	// respose.send() takes care of setting Content-Type to text/html automatically if the response is a String
-	// response.send('hello there!');
+  console.log('got request for "/hello/world"');
+  // respose.send() takes care of setting Content-Type to text/html automatically if the response is a String
+  // response.send('hello there!');
 
-	// response.set('Content-Type', 'text/plain');
-	// response.send('<h1>Hello!</h1>');
+  // response.set('Content-Type', 'text/plain');
+  // response.send('<h1>Hello!</h1>');
 
-	// response.send() takes care of setting the response's Content-Type to application/json if the sent data is an Array or Object
-	response.send({ message: 'hello there!'});
+  // response.send() takes care of setting the response's Content-Type to application/json if the sent data is an Array or Object
+  response.send({ message: 'hello there!'});
 
 });
 
@@ -20,21 +94,21 @@ app.get('/hello/world', function(request, response){
   ROUTE WITH PARAMS/DYNAMIC ROUTES
 **/
 app.get('/hello/:name', function(request, response) {
-	response.send(`hello ${request.params.name}`);
+  response.send(`hello ${request.params.name}`);
 });
 
 app.get('/users/:id', function(request, response) {
-	var userId = request.params.id;
-	console.log(`got request for '/users/${userId}'`);
+  var userId = request.params.id;
+  console.log(`got request for '/users/${userId}'`);
 
-	// in a real app we would look for user in a database
-	var users = {
-		1: 'John',
-		2: 'Sandy',
-		3: 'David'
-	};
+  // in a real app we would look for user in a database
+  var users = {
+    1: 'John',
+    2: 'Sandy',
+    3: 'David'
+  };
 
-	response.send(`Hello ${users[userId]}`);
+  response.send(`Hello ${users[userId]}`);
 });
 
 // Return a friendly greeting for firstName and lastName query params
@@ -42,17 +116,23 @@ app.get('/users/:id', function(request, response) {
 
 // http://localhost:3000/hi?firstName=Hou&lastName=Chia
 app.get('/hi', function(request, response) {
-	var firstName = request.query.firstName;
-	var lastName = request.query.lastName;
+  var firstName = request.query.firstName;
+  var lastName = request.query.lastName;
 
-	response.send(`hi ${firstName} ${lastName}`);
+  response.send(`hi ${firstName} ${lastName}`);
 });
 
 app.get('*', function(request, response) {
-	response.status(404).send('uh oh! page not found!');
+  response.status(404).send('uh oh! page not found!');
 });
 
 app.listen(3000, function() {
-	console.log('Example app is listening on port 3000!');
-})
+  console.log('Example app is listening on port 3000!');
+});
+
+
+/**
+  EXERCISE: BODY PARSING (10 minutes)
+**/
+
 
